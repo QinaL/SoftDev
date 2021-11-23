d8a564f976c622b67fecaf9f9fa2468b38857759
Qina <qina614liu@gmail.com>
2021-11-23 18:46:35 -0500

    add

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,4 +1,19 @@
-''' 
+from flask import Flask,render_template
+import urllib, json
 
-'''
 
+
+app = Flask(__name__) 
+
+
+@app.route('/')
+def main():
+    nasa = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=pJkgjfYApMvTrcMw0O7hdO7Chooqu9b4jcuDu2zC')
+    nasa_text = json.load(nasa)
+    picture = (nasa_text['url']) 
+    return render_template('main.html', pic = picture)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
