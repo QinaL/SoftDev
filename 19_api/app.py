ab46afcf8f9cac266ed8a584f2927ca930c7e6b9
Qina <qina614liu@gmail.com>
2021-11-25 22:05:46 -0500

    fixed errors found

--- 19_api/app.py
+++ 19_api/app.py
@@ -3,7 +3,7 @@
     SoftDev
     k19
     11-23-2021
-    time spent: .6 hour
+    time spent: .8 hour
 '''
 
 from flask import Flask,render_template
@@ -19,7 +19,7 @@ def main():
     nasa = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=pJkgjfYApMvTrcMw0O7hdO7Chooqu9b4jcuDu2zC')
     nasa_text = json.load(nasa)
     picture = (nasa_text['url']) 
-    return render_template('main.html', pic = picture)
+    return render_template('main.html', pic = nasa_text['url'], explanation = nasa_text['explanation'])
 
 
 if __name__ == '__main__':
