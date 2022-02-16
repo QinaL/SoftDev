15b61f8b4df4b322e6ede54b5cf0c56f7a770b26
QinaL <qina614liu@gmail.com>
2022-02-16 07:04:43 -0500

    w

--- 31_js-anim/animate.js
+++ 31_js-anim/animate.js
@@ -1,12 +1,11 @@
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon
-// SoftDev pd0
-// K31 -- canvas based JS animation
-// 2022-02-15t
+// Team (Liu)^2 :: Sophie Liu, Qina Liu
+// SoftDev pd2
+// K31 -- Paint Paint Paint...
+// 2022-02-15
 
 // model for HTML5 canvas-based animation
 
 // SKEELTON
-
 
 //access canvas and buttons via DOM
 var c = document.getElementById('playground');// GET CANVAS
@@ -25,7 +24,7 @@ var requestID;  //init global var for use with animation frames
 //var clear = function(e) {
 var clear = (e) => {
   console.log("clear invoked...")
-
+  ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
   // YOUR CODE HERE
 };
 
@@ -36,25 +35,36 @@ var growing = true;
 
 //var drawDot = function() {
 var drawDot = () => {
+  clear();
   console.log("drawDot invoked...")
-
   ctx.beginPath();
-  ctx.arc(c.clientWidth/2, c.clientHeight/2, 50, 0, 2*Math.PI);
+  ctx.arc(c.clientWidth/2, c.clientHeight/2, radius, 0, 2*Math.PI);
   ctx.fill();
   ctx.stroke();
+  if (radius == (Math.min(c.clientWidth,c.clientHeight) / 2)) {
+    growing = false;
+  }
+  if (radius == 0) {
+    growing = true;
+   }
+  if (growing) {
+    radius++;
+  }
+  else {
+    radius--;
+  }
+  requestID = window.requestAnimationFrame(drawDot);
   // YOUR CODE HERE
 
   /*
     ...to
     Wipe the canvas,
     Repaint the circle,
-
     ...and somewhere (where/when is the right time?)
     Update requestID to propagate the animation.
     You will need
     window.cancelAnimationFrame()
     window.requestAnimationFrame()
-
    */
 };
 
@@ -63,17 +73,28 @@ var drawDot = () => {
 var stopIt = () => {
   console.log("stopIt invoked...")
   console.log( requestID );
-
+  requestID = window.cancelAnimationFrame(requestID);
   // YOUR CODE HERE
   /*
     ...to
     Stop the animation
-
     You will need
     window.cancelAnimationFrame()
   */
 };
 
-
-dotButton.addEventListener( "click", drawDot );
-stopButton.addEventListener( "click",  stopIt );
+var clickity = false;
+var clickTest = function() {
+  if (!clickity) {
+    clickity = true;
+    drawDot();
+  }
+}
+var clickTest2 = function() {
+  if (clickity) {
+    clickity = false;
+    stopIt();
+  }
+}
+dotButton.addEventListener( "click", clickTest );
+stopButton.addEventListener( "click",  clickTest2 );
